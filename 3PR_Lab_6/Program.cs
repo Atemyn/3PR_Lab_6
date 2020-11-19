bddd56b758ff99efdd0ad7da23d9011b0111af4e
Atemyn <atemyn@mail.ru>
2020-11-19 16:15:46 +0300

    Пример работы с экземплярами класса

--- 3PR_Lab_6/Program.cs
+++ 3PR_Lab_6/Program.cs
@@ -7,7 +7,21 @@ namespace _3PR_Lab_6
     {
         static void Main(string[] args)
         {
+			/* Работа с экземплярами класса. */
+			Building firstBuilding = new Building();
+			Building secondBuilding = new Building();
 
-        }
+			Console.WriteLine("Программа по созданию зданий.\nВсе размерные данные вводятся в метрах.\n");
+			Console.WriteLine("Работа с экземплярами класса: ");
+
+			firstBuilding.inputBuilding();
+			secondBuilding.initBuilding();
+
+			firstBuilding.addToBuilding(secondBuilding);
+
+			firstBuilding.addFloors();
+			firstBuilding.removeFloors();
+			/*............................................*/
+		}
 	}
 }
